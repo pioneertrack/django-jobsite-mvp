9ed41a6f820e5ce17dbea0c8ab18e6fdaac869b8
Steven Motley <stevenmotley89@gmail.com>
2017-09-08 17:58:52 +0700

    New search implementation (continue) PeopleDocument implementation, Elasticsearch query structure

--- search/documents.py
+++ search/documents.py
@@ -1,4 +1,5 @@
 from django_elasticsearch_dsl import DocType, Index, fields
+from elasticsearch_dsl import Field
 from website.profile import Profile, Founder, Job
 
 people = Index('people')
@@ -11,6 +12,9 @@ people.settings(
 @people.doc_type
 class PeopleDocument(DocType):
     user = fields.ObjectField(properties={
+        'is_active': fields.BooleanField(),
+        'is_individual': fields.BooleanField(),
+        'is_account_disabled': fields.BooleanField(),
         'first_name': fields.StringField(),
         'last_name': fields.StringField(),
     })
@@ -19,11 +23,16 @@ class PeopleDocument(DocType):
         'position': fields.StringField(),
         'description': fields.TextField(),
     })
+    positions = fields.StringField()
 
     class Meta:
         model = Profile
         fields = [
+            'year',
             'major',
+            'role',
+            'has_startup_exp',
+            'has_funding_exp',
             'bio',
             'skills',
             'interests',
--- search/views.py
+++ search/views.py
@@ -1,42 +1,90 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from django import forms
 from search.documents import PeopleDocument
 from elasticsearch_dsl import FacetedSearch
+import website.profile as profile
+
+JOB_CONTEXT = {
+    'p_context': [
+        ('year', list(profile.YEAR_IN_SCHOOL_CHOICES), {'class': 'label-year', 'name': 'year'}),
+        ('major', list(profile.MAJORS), {'class': 'label-major', 'name': 'major'}),
+        ('role', list(profile.PRIMARY_ROLE), {'class': 'label-role', 'name': 'role'}),
+        ('experience', [('0', 'Has startup experience'), ('1', 'Has funding experience')],
+         {'class': 'label-experience'}),
+    ],
+    'e_context': [
+        ('position', [
+            ('1', 'Intern'),
+            ('2', 'Part-Time'),
+            ('3', 'Full-Time')
+        ], {'class': 'label-position'}),
+    ],
+    'f_context': [
+        ('stage', list(profile.STAGE), {'class': 'label-stage'}),
+        ('fields', list(profile.CATEGORY), {'class': 'label-field', 'name': 'field'})
+    ],
+    'job_context': [
+        ('category', list(profile.CATEGORY), {'class': 'label-category'}),
+        ('level', list(profile.LEVELS), {'class': 'label-level'}),
+        ('pay', list(profile.POSITION), {'class': 'label-pay'})
+    ]
+}
 
 
 # Create your views here.
-class SearchView(TemplateView):
-
+class SearchView(FormView):
     template_name = 'search_view.html'
+    form_class = forms.Form
 
     def get_context_data(self, **kwargs):
         context = super(SearchView, self).get_context_data(**kwargs)
+        context.update(JOB_CONTEXT)
+        return context
 
+    def post(self, request, *args, **kwargs):
+        category = request.POST['select-category']
+        position = request.POST.getlist('position_' + category)
+        years = request.POST.getlist('year_' + category)
+        majors = request.POST.getlist('major_' + category)
+        roles = request.POST.getlist('role_' + category)
+        experience = request.POST.getlist('experience_' + category)
 
-        res = PeopleDocument.search().query(
-            'multi_match',
-            query='John Doe',
-            type='cross_fields',
-            fields =[
-                'major',
-                'bio',
-                'skills',
-                'interests',
-                'courses',
-                'user.first_name',
-                'user.last_name',
-                'experience_set.company',
-                'experience_set.position',
-                'experience_set.description',
-            ]
-        )
+        query = {
+            'query': {
+                'bool': {
+                    'filter': [
+                        {'term': {'user.is_active': True}},
+                        {'term': {'user.is_individual': True}},
+                        {'term': {'user.is_account_disabled': False}},
+                        {'terms': {'positions': position}},
+                        {'terms': {'year': years}},
+                        {'terms': {'major': majors}},
+                        {'terms': {'role': roles}},
+                    ],
+                    'must': {
+                        'multi_match': {
+                            'query': 'John Doe',
+                            'type': 'cross_fields',
+                            'fields': [
+                                'major',
+                                'bio',
+                                'skills',
+                                'interests',
+                                'courses',
+                                'user.first_name',
+                                'user.last_name',
+                                'experience_set.company',
+                                'experience_set.position',
+                                'experience_set.description',
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+
+        res = PeopleDocument.search().from_dict(query)
 
         for hit in res:
             a = hit
-
-        # s = PeopleSearch('John Doe');
-        # res = s.execute()
-
-        context['result'] = res;
-
-        return context
