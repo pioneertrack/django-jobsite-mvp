41ac82ff58a386186d50cea8ebe5149ac9133936
Steven Motley <stevenmotley89@gmail.com>
2017-10-04 17:42:00 +0700

    landing_fixes

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -140,6 +140,14 @@ $(function(){
     }
   });
 
+	$(document).on('click','.welcome-links a', function(e) {
+		var d = new Date();
+    d.setTime(d.getTime() + (24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+		document.cookie = 'select-category=' + $(this).data('category') + ';' + expires + ';path=/'
+	});
+
+
 	setTimeout(function() {
 		$('.message').fadeOut('slow');
 	}, 3500);
