d913f2300ead46b90393de71daeaadeb90e2c6cc
Steven Motley <stevenmotley89@gmail.com>
2017-10-10 13:30:48 +0700

    Profile redirect fix

--- website/views.py
+++ website/views.py
@@ -122,10 +122,10 @@ def check_profiles(view_func):
         user = request.user
         redirect_url = None
         if user.first_login:
-            if user.is_individual and not hasattr(user, 'profile'):
+            if user.is_individual and not hasattr(user, 'profile') or user.profile.is_filled == False:
                 messages.success(request, "Welcome to BearFounders! Please tell us about yourself.")
                 redirect_url = 'website:profile_update'
-            elif user.is_founder and not hasattr(user, 'founder'):
+            elif user.is_founder and not hasattr(user, 'founder') or user.founder.is_filled == False:
                 messages.success(request, "Welcome to BearFounders! Please tell us about you startup.")
                 redirect_url ='website:startup_update'
         else:
