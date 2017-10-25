7a65935a1997e79b3a1e9454069722708656df46
Steven Motley <stevenmotley89@gmail.com>
2017-10-25 10:32:31 +0700

    Remove unused serializer

--- website/forms.py
+++ website/forms.py
@@ -14,21 +14,21 @@ import logging
 from django.utils.encoding import force_text
 import base64, uuid
 from django.core.files.base import ContentFile
-from rest_framework import serializers
+# from rest_framework import serializers
 
 log = logging.getLogger(__name__)
 logging.basicConfig(filename='errorlog.txt')
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            # base64 encoded image - decode
-            format, imgstr = data.split(';base64,') # format ~= data:image/X,
-            ext = format.split('/')[-1] # guess file extension
-            id = uuid.uuid4()
-            data = ContentFile(base64.b64decode(imgstr), name = id.urn[9:] + '.' + ext)
-        return super(Base64ImageField, self).to_internal_value(data)
+# class Base64ImageField(serializers.ImageField):
+#     def to_internal_value(self, data):
+#         if isinstance(data, str) and data.startswith('data:image'):
+#             # base64 encoded image - decode
+#             format, imgstr = data.split(';base64,') # format ~= data:image/X,
+#             ext = format.split('/')[-1] # guess file extension
+#             id = uuid.uuid4()
+#             data = ContentFile(base64.b64decode(imgstr), name = id.urn[9:] + '.' + ext)
+#         return super(Base64ImageField, self).to_internal_value(data)
 
 
 class NewRegistrationForm(RegistrationFormUniqueEmail):
