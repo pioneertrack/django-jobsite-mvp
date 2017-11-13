1fce1b6b558b768602b7d7c9bebaf9b83346a309
Steven Motley <stevenmotley89@gmail.com>
2017-11-13 16:02:52 +0700

    Issue #28 Template for internal page

--- content/views.py
+++ content/views.py
@@ -8,7 +8,7 @@ from content.models import Story
 # Create your views here.
 class StoryListView(ListView):
     model = Story
-
+    queryset = Story.objects.filter(published=True).order_by('-created_at')
 
 class StoryDetailView(DetailView):
     model = Story
