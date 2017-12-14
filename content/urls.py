5b60f1f97724b6b91cc4838cc5ea1fa09ea04776
Steven Motley <stevenmotley89@gmail.com>
2017-12-14 16:34:52 +0700

    Issue #38 Resources to Templates change Templates changes

--- content/urls.py
+++ content/urls.py
@@ -4,7 +4,7 @@ from content.views import *
 urlpatterns = [
     url(r'^stories/$', StoryListView.as_view(), name="stories"),
     url(r'^story/(?P<slug>[\w-]+)/$', StoryDetailView.as_view(), name="story_detail", ),
-    url(r'^resources/$', ResourceListView.as_view(), name="resources"),
-    url(r'^resource/(?P<slug>[\w-]+)/$', ResourceDetailView.as_view(), name="resource_detail", ),
+    url(r'^templates/$', ResourceListView.as_view(), name="templates"),
+    url(r'^template/(?P<slug>[\w-]+)/$', ResourceDetailView.as_view(), name="template_detail", ),
     url(r'^picture-autocomplete/$', PictureAutocomplete.as_view(), name='picture-autocomplete', ),
 ]
